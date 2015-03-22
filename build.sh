4b1521ff002108534d0503a65b5e8e25b188b5b8
jimy-byerley <jimy.byerley@gmail.com>
2015-03-22 10:54:51 +0100

    linux script to optimize game disk space after installation : remove useless files

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "$1" == "-h" ]; then 
+	echo "simple sh script used to optimize the game installation.
+This program will remove several files that are useless for pure game runtime.
+useless files
+	.xcf	gimp files (textures)
+	.blend1	blender backups files
+	all files or directory found in projects.lst
+use:
+	./build.sh [PATH]
+"
+	return
+fi
+
+extensions='*.xcf *.blend1'
+
+for ext in $extensions; do
+	listfiles=$(find . -name "$ext")
+	for filename in $listfile; do
+		rm $filename
+	done
+done
+
+listfile=$(cat projects.lst)
+for filename in $listfile; do
+	rm -r $filename
+done
