74965ee35ba7b813e56d4bb59153ce1c82ef1b08
jimy-byerley <jimy.byerley@gmail.com>
2015-07-03 11:58:27 +0200

    processing for server requests done

--- network/client.py
+++ network/client.py
@@ -18,19 +18,83 @@ This file is part of Tron-R.
 """
 
 import bge
-import threading, socket, time
-import network
+import threading, socket, time, pickle
+from network import *
 
 class Client(socket.socket):
-	packet_size = 1024
+	packet_size   = 1024     # max size of buffers to receive from the server
+	update_period = 1.0      # minimum time interval between 2 update from the server, of objects positions
 	
-	def __init__(self, remote):
+	next_update = 0          # next time to ask the server for update informations
+	
+	def __init__(self, remote, scene=None, user="", password=""):
 		self.remote = remote
+		self.username = user
+		self.password = password
+		self.scene = scene or bge.logic.getCurrentScene()
 		socket.socket.__init__(self, socket.AF_INET, socket.SOCK_DGRAM)
 		socket.connect(remote)
 	
 	def step(self):
-		
+		while time.time() < self.next_update:
+			packet, host = recvfrom(self.packet_size)
+			# only packets from the server are accepted (limit intrusion)
+			while host != self.remote:   packet, host = recvfrom(self.packet_size)
+			
+			# count number of '\0' in the packet, it indicates the minimal number of words in the packet
+			zeros = packet.count(b'\0')
+			if similar(packet, b'getmeca\0') and zeros >= 1:
+				obname = packet.split(b'\0')[1]
+				ob = obname.decode()
+				if ob in self.scene.objects:
+					obj = self.scene.objects[ob]
+					if obj.parent:  parent = obj.parent.name
+					else:           parent = None
+					msg = b'setmeca\0' + obname + b'\0' + pickle.dumps((
+						obj.worldPosition,        obj.worldOrientation,
+						obj.worldLinearVelocity,  obj.worldAngularVelocity,
+						parent))
+					self.send(msg)
+				else:
+					self.send(b'unknown\0'+obname)
+			
+			elif similar(packet, b'getprop\0') and zeros >= 2:
+				obname, propname = packet.split(b'\0')[1:3]
+				ob   = obname.decode()
+				prop = propname.decode()
+				if ob in self.scene.objects 
+					if prop in self.scene.objects[ob] :
+					msg = b'setpro\0' + obname + b'\0'+ propname + b'\0' + pickle.dumps(self.objects[ob][prop])
+					self.sendto(msg), host)
+				else:
+					self.send(b'unknown\0'+obname)
+			
+			# packet of kind:    setmeca.name.dump  ('\0' instead of .)
+			else similar(packet, b'setmeca\0' and zeros >= 1: 
+				obname = packet.split(b'\0')[1]
+				ob = obname.decode()
+				if ob in self.scenes.objects:
+					obj = self.scenes.objects[ob]
+					obj.worldPosition,        obj.worldOrientation,
+					obj.worldLinearVelocity,  obj.worldAngularVelocity,
+					obj.parent     = pickle.loads(packet[9+len(obname):])
+			
+			# packet of kind:    setprop.name.propertyname.dump   ('\0' instead if .)
+			elif similar(packet, b'setprop\0' and zeros >= 2:
+				obname, propname = packet.split(b'\0')[1:3]
+				ob = obname.decode()
+				if obname in self.scenes.objects:
+					self.scenes.objects[ob][propname.decode()] = packet[:10+len(obname)+len(propname)]
+					
+			
+			elif similar(packet, PACKET_STOP):
+				self.close()
+				return
+	
+	def thread_step(self):
+		self.thread = threading.Thread()
+		self.thread.run = self.set
+		self.thread.start()
 	
 	# clear the socket's queue, return the list of packet received
 	def clear_requests(self):
