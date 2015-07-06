336339a35ae0e0bd544579249015c32663d3526a
jimy-byerley <jimy.byerley@gmail.com>
2015-07-06 11:23:28 +0200

    basic synchronisation done (mechanic properties and game proprties)

--- network/network.py
+++ network/network.py
@@ -29,12 +29,12 @@ def debugmsg(*args):
 def similar(s, pattern) :        
 	return s[:len(pattern)] == pattern
 
-def username_conformity_off(s) : 
+def username_conformity_off(_, s) : 
 	return True
 
 
 
-PACKET_STOP    = b"quitting"
+PACKET_STOP    = b"quitting\0"
 
 class obdata:
 	"""
@@ -43,10 +43,13 @@ class obdata:
 	host = 0               # maximal index of host who can provide information about this object
 	updated = False        # marker for 'updated during last cycle'
 	
-	physics = None      # dump of physics properties of the object, None means physic properties not synchronized.
+	physics = False     # dump of physics properties of the object, False means physic properties not synchronized, can be None when sync is imminent.
 	properties = {}     # dumps of game object properties of the object, only references properties are inside.
 	                    # properties names are bytes (for optimisation)
+	                    # property take value ignore (see class below) when sync is imminent the first time.
 
+class ignore:
+	pass
 
 """
 * chaque hote peut ajouter a la liste des objets a synchronisée, tenue par le serveur
@@ -60,9 +63,10 @@ class obdata:
 class Server(socket.socket):
 	packet_size          = 1024
 	max_client           = 30      # maximum number of clients (can be set at runtime to increase number of clients but doesn't quick connected clients
-	update_period     = 0.5     # maximum time (s) between 2 server update communication (update of client datas)
+	update_period        = 0.1     # maximum time (s) between 2 server update communication (update of client datas)
+	step_time            = 0.2     # maximum time in execution of each step
 	bad_password_timeout = 3       # time the client should wait when get a wrong password (second)
-	multiple_sessions    = False   # set to True, allow multiple host to use the same user (account and password)
+	multiple_sessions    = True    # set to True, allow multiple host to use the same user (account and password)
 	registeration        = True    # set to True, allow new users to be created
 	username_conformity  = username_conformity_off # function to call to test a new username (valid or not)
 	
@@ -79,6 +83,7 @@ class Server(socket.socket):
 	
 	next_update = 0        # next time to update database
 	cleared_index = []     # index of freed clients (in lists)
+	answering_clients = [] # list of booleans associated with index of clients (True if the client has send a request after the last update)
 	
 	
 	# put lan to False if you don't want to use lan interface
@@ -99,121 +104,154 @@ class Server(socket.socket):
 	
 	# server step to tread all client requests
 	def step(self):
-		while time.time() < self.next_update :
+		self.run = True
+		end_step = time.time() + self.step_time
+		while time.time() < self.next_update and time.time() < end_step and self.run :
 			# thread client requests
-			packet, host = recvfrom(self.packet_size)
-			# wait for quicked or delayed hosts (bad password for exemple)
-			while host in self.delays and time.time() > self.delays[host]:
-				packet, host = recvfrom(self.packet_size)
-	
-			# no communication with a not identified host
-			if host in self.hosts:
-				index = self.hosts.find(host)
-				# count number of '\0' in the packet, it indicates the minimal number of words in the packet
-				zeros = packet.count(b'\0')
-				if similar(packet, b'getmeca\0') and zeros >= 1:
-					obname = packet.split(b'\0')[1]
-					if obname in self.datas:
-						data = self.datas[obname]
-						if data.physics:
-							msg = b'setmeca\0' + obname + b'\0' + data.physics
-							self.sendto(msg, host)
-				
-				elif similar(packet, b'getprop\0') and zeros >= 2:
-					obname, propname = packet.split(b'\0')[1:3]
-					if obname in self.datas and propname in self.datas[obname].properties :
-						msg = b'setpro\0' + obname + b'\0'+ propname + b'\0' + self.datas[obname].properties[propname]
-						self.sendto(msg), host)
-				
-				elif similar(packet, b'unknown\0') and zeros >= 1:
-					obname = packet.split(b'\0')[1]
-					if obname in self.datas and index == self.datas[obname].host:
-						self.datas[obname].host = None
-						data.host = None
-				
-				# packet of kind:    setmeca.name.dump  ('\0' instead of .)
-				else similar(packet, b'setmeca\0' and zeros >= 1: 
-					obname = packet.split(b'\0')[1]
-					if obname in self.datas:
-						data = self.datas[obname]
-						# if this host is more trusted, this host will be in charge of this data
-						if data.host == None or data.host <= index:
-							data.updated = True
-							data.host = index
-							data.physics = packet[:len(obname)])      
-							self.datas[obname] = data
-				
-				# packet of kind:    setprop.name.propertyname.dump   ('\0' instead if .)
-				elif similar(packet, b'setprop\0' and zeros >= 2:
-					obname, propname = packet.split(b'\0')[1:3]
-					if obname in self.datas:
-						data = self.datas[obname]
-						# if this host is more trusted, this host will be in charge of this data
-						if data.host == None or data.host <= index:
-							data.updated = True
-							data.host = index
-							data.properties[propname] = packet[10+len(obname)+len(propname):]
-							self.datas[obname] = data
-					 
-				
-				elif similar(packet, b'disconnect\0'):
-					self.remove_client(host)
-			
-			# try to resolve host, or reject it
-			else: 
-				if similar(packet, b'authentify\0'):
-					user, password = packet.split(b'\0')[1:3]
-					user = user.decode()
-					password = password.decode()
-					
-					# if client is not known, register it (except configuration)
-					if user not in self.passwords and self.registeration:
-						if self.username_conformity(user):
-							debugmsg('create user \'%s\'.' % user)
-							self.passwords[user] = password
-						else:
-							debugmsg('new username \'%s\' rejected because of unconformity.' % user)
-							send(b'username not conform', host)
-							break
-					else:
-						debugmsg('creation of new user is forbidden.')
-						send(b'new user disallowed', host)
-						break
-					
-					# user should be created
-					if password == self.passwords[user]:
-						if host in self.hosts and not self.multiple_sessions:
-							debugmsg('an other session for host %s refused.' % host[0])
-							self.sendto(b'multisession not allowed', host)
-						if self.num_client >= self.max_client:
-							debugmsg('new session for host %s refused: server is full.', % host[0])
-							self.sendto(b'server is full', host)
-						else:
-							debugmsg('new session for host %s.' % host[0])
-							self.sendto(b'password accepted')
-							self.add_client(host)
-					else:
-						self.delays[host] = time.time() + self.bad_password_timeout
-						self.send(b'password rejected', host)
-						
-		for name in range(len(self.datas)):
-			self.datas[name].updated = None
-			data = self.datas[name] # set marker to false, to detect if client doesn't answer
-			packets = [] # list of packets to send to clients concerned by this object
-			
-			if data.position or data.rotation or data.parent or data.velocity or data.angular :
-				packets.append(b'getmeca\0'+name.encode()+b'\0')
-			if data.properties :
-				for prop in data.properties and data.properties[prop]:
-					packets.append(b'getprop\0'+name.encode()+b'\0'+prop.encode()+b'\0')
-			# else, send request to this client and more trusted clients
-			if data.host and self.hosts[data.host]:
-				for host in self.hosts[:data.host+1]:
-					if host: self.sendto(packet, host)
-			# if an host is disconnected or away from this object take an other host
+			try:    packet, host = self.recvfrom(self.packet_size)
+			except: time.sleep(0.001)
 			else:
-				for packet in packets:
-					self.send(packet, host)
+				# wait for quicked or delayed hosts (bad password for exemple)
+				while host in self.delays and time.time() < self.delays[host]:
+					packet, host = recvfrom(self.packet_size)
+		
+				print(self.hosts)
+				# no communication with a not identified host
+				if host in self.hosts:
+					index = self.hosts.index(host)
+					# count number of '\0' in the packet, it indicates the minimal number of words in the packet
+					zeros = packet.count(b'\0')
+					if similar(packet, b'getmeca\0') and zeros >= 1:
+						obname = packet.split(b'\0')[1]
+						if obname in self.datas:
+							data = self.datas[obname]
+							if data.physics:
+								# if origin of the sync is updated by the sync, the object is frozen
+								if index != data.host:
+									msg = b'setmeca\0' + obname + b'\0' + data.physics
+									self.sendto(msg, host)
+							else:
+								self.datas[obname].physics = None
+						else:
+							self.datas[obname] = obdata()
+							self.datas[obname].physics = None
+						self.answering_clients[index] = True
+					
+					elif similar(packet, b'getprop\0') and zeros >= 2:
+						obname, propname = packet.split(b'\0')[1:3]
+						if obname in self.datas:
+							if propname in self.datas[obname].properties :
+								# if origin of the sync is updated by the sync, the object is frozen
+								if index != self.datas[obname].host and self.datas[obname].properties[propname] != ignore:
+									msg = b'setprop\0' + obname + b'\0'+ propname + b'\0' + self.datas[obname].properties[propname]
+									self.sendto(msg, host)
+							else: 
+								self.datas[obname].properties[propname] = ignore
+						else: 
+							self.datas[obname] = obdata()
+							self.datas[obname].properties[propname] = ignore
+						self.answering_clients[index] = True
+					
+					elif similar(packet, b'unknown\0') and zeros >= 1:
+						obname = packet.split(b'\0')[1]
+						if obname in self.datas and index == self.datas[obname].host:
+							self.datas[obname].host = None
+							data.host = None
+					
+					# packet of kind:    setmeca.name.dump  ('\0' instead of .)
+					elif similar(packet, b'setmeca\0') and zeros >= 1: 
+						obname = packet.split(b'\0')[1]
+						if obname in self.datas:
+							data = self.datas[obname]
+							# if this host is more trusted, this host will be in charge of this data
+							if data.host == None or data.host <= index:
+								data.updated = True
+								data.host = index
+								data.physics = packet[9+len(obname):]
+								self.datas[obname] = data
+					
+					# packet of kind:    setprop.name.propertyname.dump   ('\0' instead if .)
+					elif similar(packet, b'setprop\0') and zeros >= 2:
+						obname, propname = packet.split(b'\0')[1:3]
+						if obname in self.datas:
+							data = self.datas[obname]
+							# if this host is more trusted, this host will be in charge of this data
+							if data.host == None or data.host <= index:
+								data.updated = True
+								data.host = index
+								data.properties[propname] = packet[10+len(obname)+len(propname):]
+								self.datas[obname] = data
+						
+					
+					elif similar(packet, PACKET_STOP):
+						self.remove_client(host)
+				
+				# try to resolve host, or reject it
+				else: 
+					if similar(packet, b'authentify\0'):
+						user, password = packet.split(b'\0')[1:3]
+						user = user.decode()
+						password = password.decode()
+						subject = b'authentication\0'
+						
+						# if client is not known, register it (except configuration)
+						if user not in self.passwords:
+							if self.registeration:
+								if self.username_conformity(user):
+									debugmsg('create user \'%s\'.' % user)
+									self.passwords[user] = password
+								else:
+									debugmsg('new username \'%s\' rejected because of unconformity.' % user)
+									sendto(subject + b'username not conform', host)
+									break
+							else:
+								debugmsg('creation of new user is forbidden.')
+								self.sendto(subject + b'new user disallowed', host)
+								break
+						
+						# user should be created
+						if password == self.passwords[user]:
+							if host in self.hosts and not self.multiple_sessions:
+								debugmsg('an other session for host %s refused.' % host[0])
+								self.sendto(subject + b'multisession not allowed', host)
+							if self.num_client >= self.max_client:
+								debugmsg('new session for host %s refused: server is full.' % host[0])
+								self.sendto(subject + b'server is full', host)
+							else:
+								debugmsg('new session for host %s.' % host[0])
+								self.sendto(subject + b'password accepted', host)
+								self.add_client(host, user)
+						else:
+							self.delays[host] = time.time() + self.bad_password_timeout
+							self.sendto(subject + b'password rejected', host)
+		
+		if time.time() < end_step and self.run:
+			for name in self.datas:
+				self.datas[name].updated = None
+				data = self.datas[name] # set marker to false, to detect if client doesn't answer
+				packets = [] # list of packets to send to clients concerned by this object
+				
+				# maybe send a maximal sized packet of data to unknown hosts (big packet to make a DOS on the machine thaht try to make one)
+				
+				if data.physics != False :
+					packets.append(b'getmeca\0'+ name +b'\0')
+				if data.properties :
+					for prop in data.properties:
+						packets.append(b'getprop\0'+ name +b'\0'+ prop +b'\0')
+				# else, send request to this client and more trusted clients
+				if data.host and self.hosts[data.host]:
+					for packet in packets:
+						for i in range(data.host+1):
+							host = self.hosts[i]
+							if host and self.answering_clients[i]: self.sendto(packet, host)
+				# if an host is disconnected or away from this object take an other host
+				else:
+					for packet in packets:
+						for i in range(len(self.hosts)):
+							host = self.hosts[i]
+							if host and self.answering_clients[i]: self.sendto(packet, host)
+			self.next_update = time.time() + self.update_period
+			self.answering_clients = len(self.answering_clients)* [False]
 	
 	
 	# execute self.step() in an other thread
@@ -233,8 +271,9 @@ class Server(socket.socket):
 	
 	# stop the server
 	def stop(self):
-		self.sendall(PACKETSTOP)
-		self.running = False
+		for host in self.hosts:
+			if host: self.sendto(PACKET_STOP, host)
+		self.run = False
 	
 	# declare a new client (without authenticate him) and return host index (or -1 if fails).
 	# user is the username
@@ -244,26 +283,26 @@ class Server(socket.socket):
 		if self.cleared_index:
 			index = self.cleared_index.pop(0)
 			self.hosts[index] = host
-			self.delays[index] = 0.
-		
+			self.answering_clients[index] = True
 		elif self.num_client <= self.max_client:
 			index = len(self.hosts)
-			self.hosts.append(index)
-			self.delays.append(0.)
-		
+			self.hosts.append(host)
+			self.answering_clients.append(True)
 		else:
 			return -1
+		self.delays[index] = 0
 		self.order.insert(trusting, index)
-			self.users[user].append(host)
+		if user not in self.users:  self.users[user] = []
+		self.users[user].append(host)
 		self.num_client += 1
 		return index
 	
 	def remove_client(self, host):
 		if host in self.hosts:
-			index = self.hosts.find(host)
-			self.sendto(b'disconnect', host)
+			index = self.hosts.index(host)
+			self.sendto(PACKET_STOP, host)
 			self.hosts[index] = None
-			self.order.pop(self.order.find(index))
+			self.order.pop(self.order.index(index))
 			self.num_client -= 1
 			self.cleared_index.append(index)
 			return index
