48c359952dc7168a26dd23da17ca290c3a1d8892
jimy-byerley <jimy.byerley@gmail.com>
2015-07-06 11:34:55 +0200

    no change

--- Tron-R.bat
+++ Tron-R.bat
@@ -1,7 +1,11 @@
 @echo off
 
-if exist softwares\blender (
-	softwares\blender\blenderplayer.exe scenes\menu.blend
+if exist blenderplayer_path.txt (
+	set /P path="" < blenderplayer_path.txt
+	set blenderplayer=%path%\blenderplayer.exe
+	if exist %blenderplayer% (
+		%blenderplayer% scenes/main.blend
+	)
 ) else (
 	echo Game have not been builded !! run "build.bat" before.
 	echo press Q to close.
