d3aedf139378fe420f097ddd627fa0a9c406dfaf
jimy-byerley <jimy.byerley@gmail.com>
2015-07-15 01:24:52 +0200

    slashes are replaced by backslashes (stupid linuxian error), from issue 1

--- Tron-R.bat
+++ Tron-R.bat
@@ -1,7 +1,7 @@
 @echo off
 
-if exist softwares/blender (
-	softwares/blender/blenderplayer scenes/main.blend
+if exist softwares\blender (
+	softwares\blender\blenderplayer scenes\main.blend
 ) else (
 	echo Game have not been built !! run "build.bat" before.
 	echo press Q to close.
