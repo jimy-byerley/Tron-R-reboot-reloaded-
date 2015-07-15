655e60df3d150ee7cae59b37c8c6b6016f83982d
jimy-byerley <jimy.byerley@gmail.com>
2015-07-15 09:42:29 +0200

    corrected: menu.blend is opened instead of main.blend

--- Tron-R.bat
+++ Tron-R.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist softwares\blender (
-	softwares\blender\blenderplayer scenes\main.blend
+	softwares\blender\blenderplayer scenes\menu.blend
 ) else (
 	echo Game have not been built !! run "build.bat" before.
 	echo press Q to close.
