fd247e143b4cbd716f830016d3588b30b0bc9c0c
jimy-byerley <jimy.byerley@gmail.com>
2015-07-18 16:08:56 +0200

    help added for command line

--- mainscene_loader.sh
+++ mainscene_loader.sh
@@ -4,6 +4,17 @@
 PULSE_LATENCY_MSEC=
 export PULSE_LATENCY_MSEC
 
-# launch
-$(cat blenderplayer_path.txt)/blenderplayer scenes/main.blend "$@"
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+	echo "usage: mainscene_loader.sh [BLENDER OPTIONS] - [GAME OPTIONS] -l BACKUP_FILE
+BLENDER OPTIONS:
+-w  WIDTH HEIGHT     set window width and height
+-f                   start in fullscreen mode
+-g [show_framerate = 1] [show_properties = 1] [show_profile = 1]  some debug options
 
+GAME OPTIONS:
+-n  ADDRESS PORT USERNAME PASSWORD   connect to a server
+"
+else
+	# launch
+	$(cat blenderplayer_path.txt)/blenderplayer scenes/main.blend "$@" 
+fi
