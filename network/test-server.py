6d0867cee05a85d05e3f1bb0479e27acb87bf49b
jimy-byerley <jimy.byerley@gmail.com>
2015-07-18 15:33:10 +0200

    simple file to start a game server, for test

---
+++ network/test-server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys, threading
+
+sys.path.append('.')
+
+import network
+
+port = 30000
+if len(sys.argv) > 1: port = int(argv[1])
+server = network.Server(port)
+server.setblocking(False)
+run = True
+
+def f():
+	while run:
+		server.step()
+
+t = threading.Thread()
+t.run = f
+t.start()
+
+input('type enter to stop.')
+
+run = False
+server.stop()
+t.join()
+server.close()
