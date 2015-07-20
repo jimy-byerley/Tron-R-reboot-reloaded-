48cb3412948c0383867c7cf37836ef7edcfd430b
jimy-byerley <jimy.byerley@gmail.com>
2015-07-20 09:26:21 +0200

    don't try to shutdown client on quitting local game

--- scripts/quit_game.py
+++ scripts/quit_game.py
@@ -3,6 +3,9 @@ import scenes
 
 owner = bge.logic.getCurrentController().owner
 
+if bge.logic.client:
+    bge.logic.client.stop()
+
 if bge.logic.canstop == 0:
 	print('quitting')
 	bge.logic.endGame()
