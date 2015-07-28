cb4506c549f83a24fe1b779acb6fa94ab3d55a71
jimy-byerley <jimy.byerley@gmail.com>
2015-07-28 23:36:31 +0200

    problem with mirror found: in the blender window, the world above the mirror is occluded by the plane equation, in the game, the plane equation is bad

--- shaders/grid_reflection.py
+++ shaders/grid_reflection.py
@@ -11,7 +11,7 @@ objlist = scene.objects
 
 reflsize = 512 #reflection tex dimensions
 refrsize = 512 #refraction tex dimensions
-offset = 200.0 #geometry clipping offset
+offset = 0.2 #geometry clipping offset, for hide the geometry under the grid
 
 #texture background color
 bgR = 0.02
@@ -35,25 +35,42 @@ m2.invert()
 r180 = Matrix.Rotation(radians(180),3,'Y')
 unmir = Matrix.Scale(-1,3,Vector([1,0,0]))
 
-#disable visibility for the water surface during texture rendering
+# disable visibility for the water surface during texture rendering
 own.visible = False
 
 ###REFLECTION####################
 
-#initializing camera for reflection pass
+# initializing camera for reflection pass
 pos = (viewer.position - own.position)*m1
-watercamera.position = own.position + pos*r180*unmir*m2
+pos = own.position + pos*r180*unmir*m2
 ori = Matrix(viewer.orientation)
 ori.transpose()
 ori = ori*m1*r180*unmir*m2
 ori.transpose()
-watercamera.orientation = ori
 
-#culling front faces as the camera is scaled to -1
+# delay reduction using delta offset
+if 'oldori' not in own:
+	own['oldori'] = ori
+	own['oldpos'] = pos
+	own['deltaori'] = own['oldori']-ori
+	own['deltapos'] = own['oldpos']-pos
+    
+own['deltaori'] = own['oldori']-ori
+own['deltapos'] = own['oldpos']-pos
+
+#orienting and positioning the reflection rendercamera
+watercamera.orientation = ori - own['deltaori']
+watercamera.position = pos - own['deltapos']
+
+#storing the old orientation and position of the camera
+own['oldori'] = ori
+own['oldpos'] = pos
+
+# culling front faces as the camera is scaled to -1
 bgl.glCullFace(bgl.GL_FRONT)
 
 #plane equation
-normal = own.getAxisVect((0.0, 0.0, 1.0)) #plane normals Z=front
+normal = Vector((0., 0., 1.)) * own.worldTransform # get Z axis vector of the plane in world coordinates
 
 D = -own.position.project(normal).magnitude #closest distance from center to plane
 V = (activecam.position-own.position).normalized().dot(normal) #VdotN to get frontface/backface
@@ -63,21 +80,22 @@ if V<0:
 	normal = -normal
 
 
-#making a clipping plane buffer
+# making a clipping plane buffer
 plane = bgl.Buffer(bgl.GL_DOUBLE, [4], [-normal[0], -normal[1], -normal[2], -D+offset])
 bgl.glClipPlane(bgl.GL_CLIP_PLANE0, plane)
 bgl.glEnable(bgl.GL_CLIP_PLANE0)
 
-#rendering the reflection texture in tex channel 0
-if not hasattr(g, 'reflection2'):
-	g.reflection2 = texture.Texture(own, 0, 0)
-	g.reflection2.source = texture.ImageRender(scene,watercamera)
-	g.reflection2.source.capsize = [reflsize,reflsize]
-	g.reflection2.source.background = [int(bgR*255),int(bgG*255),int(bgB*255),int(bgA*255)]
+# rendering the reflection texture in tex channel 0
+if 'reflection' not in own:
+	reflection = texture.Texture(own, 0, 0)
+	reflection.source = texture.ImageRender(scene,watercamera)
+	reflection.source.capsize = [reflsize,reflsize]
+	reflection.source.background = [int(bgR*255),int(bgG*255),int(bgB*255),int(bgA*255)]
+	own['reflection'] = reflection
 
-g.reflection2.refresh(True)
+own['reflection'].refresh(True)
 
-#restoring face culling to normal and disabling the geometry clipping
+# restoring face culling to normal and disabling the geometry clipping
 bgl.glCullFace(bgl.GL_BACK)
 bgl.glDisable(bgl.GL_CLIP_PLANE0)
 
