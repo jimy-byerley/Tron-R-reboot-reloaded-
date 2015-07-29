654ee5b36fc6a3ba7378580fbc5125ef782068d2
jimy-byerley <jimy.byerley@gmail.com>
2015-07-29 14:00:28 +0200

    restart the launcher after the game

--- scripts/quit_game.py
+++ scripts/quit_game.py
@@ -5,8 +5,8 @@ import scenes
 owner = bge.logic.getCurrentController().owner
 
 # restart parent process
-if 'game_launcher_stop' in bge.logic.config and bge.logic.config['game_launcher_stop']:
-    os.kill(os.getppid(), signal.SIGCONT)
+if bge.logic.launcher_process != None and 'game_launcher_stop' in bge.logic.config and bge.logic.config['game_launcher_stop']:
+    os.kill(bge.logic.launcher_process, signal.SIGCONT)
 
 # stop client
 if bge.logic.client:
